7e1920629445a735322c6493f25781f1b0d0de99
Agung Kusaeri <62395267+agunguf7@users.noreply.github.com>
2022-09-19 22:22:07 +0700

    Create calc.js

---
+++ js/calc.js
@@ -0,0 +1,36 @@
+function hitung(operator) {
+  let form = document.getElementById("calcForm");
+  let angka1 = parseFloat(form.angka1.value);
+  let angka2 = parseFloat(form.angka2.value);
+  let hasil = 0;
+
+  
+  if (angka1 === "" || angka2 === "")
+    alert("Angka 1 dan Angka 2 harus diisi!.");
+  if (isNaN(angka1) || isNaN(angka2)) {
+    alert("Angka 1 dan Angka 2 harus harus diisi dengan angka!.");
+  } else {
+    switch (operator) {
+      case "tambah":
+        hasil = angka1 + angka2;
+        break;
+      case "kurang":
+        hasil = angka1 - angka2;
+        break;
+      case "kali":
+        hasil = angka1 * angka2;
+        break;
+      case "bagi":
+        hasil = angka1 / angka2;
+        break;
+      case "pangkat":
+        hasil = angka1 ** angka2;
+        break;
+      default:
+        hasil = "Error";
+    }
+
+    console.log(hasil);
+    document.getElementById('hasil2').value = hasil;
+  }
+}
